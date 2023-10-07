5d029cf8966b4488adc0f6f61e88e41828a7a55d
simon <semyonkrutolevich@gmail.com>
2023-10-06 22:20:01 -0400

    add python script to test cluster connection

--- main.py
+++ main.py
@@ -7,21 +7,34 @@ pip install redis-py-cluster
 from rediscluster import RedisCluster
 
 
+HOST="X.X.X.X"
 
-ssl_certfile="./tls/ca.crt"
-ssl_keyfile="./tls/ca.key"
-ssl_ca_certs=ssl_certfile
+startup_nodes=[
+    {"host": HOST, "port": 6379 },
+    {"host": HOST, "port": 6380 }, 
+    {"host": HOST, "port": 6381 }, 
+    {"host": HOST, "port": 6382 }, 
+    {"host": HOST, "port": 6383 },
+    {"host": HOST, "port": 6384 }
+]
 
+host_port_remap=[
+    {'from_host': '127.0.0.1', 'from_port': 6379, 'to_host': HOST, 'to_port': 6379},
+    {'from_host': '127.0.0.1', 'from_port': 6380, 'to_host': HOST, 'to_port': 6380},
+    {'from_host': '127.0.0.1', 'from_port': 6381, 'to_host': HOST, 'to_port': 6381},
+    {'from_host': '127.0.0.1', 'from_port': 6382, 'to_host': HOST, 'to_port': 6382},
+    {'from_host': '127.0.0.1', 'from_port': 6383, 'to_host': HOST, 'to_port': 6383},
+    {'from_host': '127.0.0.1', 'from_port': 6384, 'to_host': HOST, 'to_port': 6384}
+]
+
+
+ssl_certfile="./tls/redis.crt"
+ssl_keyfile="./tls/redis.key"
+ssl_ca_certs="./tls/ca.crt"
 
 r = RedisCluster(
-    startup_nodes=[
-        {"host": "127.0.0.1", "port": 6379 },
-        {"host": "127.0.0.1", "port": 6381 }, 
-        {"host": "127.0.0.1", "port": 6380 }, 
-        {"host": "127.0.0.1", "port": 6382 }, 
-        {"host": "127.0.0.1", "port": 6383 },
-        {"host": "127.0.0.1", "port": 6384 }
-    ],
+    startup_nodes=startup_nodes,
+    host_port_remap=host_port_remap,
     password="12345",
     decode_responses=True,
     ssl=True,
@@ -31,5 +44,4 @@ r = RedisCluster(
     ssl_ca_certs=ssl_ca_certs,
 )
 
-print(r.cluster_nodes)
-print(r.set("a", "b"))
+print(r.ping())
